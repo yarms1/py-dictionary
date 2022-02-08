df687845bd07e2c250dbee7a3abcb223d54b4db8
IvanRamyk <ivan.r@mate.academy>
2022-02-08 10:52:08 +0200

    add comment

--- app/point.py
+++ app/point.py
@@ -9,6 +9,11 @@ class Point:
         return self.x == other.x and self.y == other.y
 
     def __hash__(self):
+        # Change the implementation of the hash to debug your code.
+        # For example, you can return self.x + self.y as a hash
+        # which is NOT a best practice, but you will be able to predict
+        # a hash value by coordinates of the point and its index
+        # in the hashtable as well
         return hash((self.x, self.y))
 
     @property
