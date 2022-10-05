e3dd833e6d5d34604b222c83df8dd429689fef48
Danylo Tiutiushkin <48014331+danylott@users.noreply.github.com>
2022-10-05 17:51:43 +0300

    Update annotations for point.py

--- app/point.py
+++ app/point.py
@@ -1,14 +1,17 @@
+from typing import Any
+
+
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x: float, y: float) -> None:
         self._x = x
         self._y = y
 
-    def __eq__(self, other):
+    def __eq__(self, other: Any) -> bool:
         if not isinstance(other, Point):
             return False
         return self.x == other.x and self.y == other.y
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         # Change the implementation of the hash to debug your code.
         # For example, you can return self.x + self.y as a hash
         # which is NOT a best practice, but you will be able to predict
@@ -17,9 +20,9 @@ class Point:
         return hash((self.x, self.y))
 
     @property
-    def x(self):
+    def x(self) -> float:
         return self._x
 
     @property
-    def y(self):
+    def y(self) -> float:
         return self._y
